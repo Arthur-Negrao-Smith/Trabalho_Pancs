e3ac9f8c4bdd35490a0baa43c42e9ba247a68c09
Arthur Negrão <arthurnegrao10s@gmail.com>
2024-10-11 21:23:23 -0300

    Adicionada a função de imprimir menu

--- panc.py
+++ panc.py
@@ -1,6 +1,18 @@
 DIAS_CONSUMO = 'dias para consumo'
 TAMANHO = 'tamanho'
 
+def print_menu() -> None:
+    menu = """
+    ------ Lista de Pancs ------
+    1. Visualizar lista completa
+    2. Visualizar um item da lista
+    3. Visualizar um trecho da lista
+    4. Inserir novo item na lista
+    Digite uma opção:
+    """
+    print(menu)
+
+
 def inserir_panc(lista_pancs:dict, nome:str, dias_consumo:int, tamanho:float) -> None:
     lista_pancs[nome] = {DIAS_CONSUMO: dias_consumo, TAMANHO:tamanho}
     return
@@ -15,3 +27,5 @@ print(lista_pancs['jambu'][TAMANHO])
 
 inserir_panc(lista_pancs, 'peixinho da orta', 123, 0.1)
 print(lista_pancs)
+
+print_menu()
